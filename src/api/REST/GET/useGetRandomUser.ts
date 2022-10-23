d0406780222fbccf8edf6de1c143e63839b29986
jmalisz <jakub.maliszewski.p@gmail.com>
2022-10-23 18:10:44 +0200

    Added full typing for useGetRandomUser

--- src/api/REST/GET/useGetRandomUser.ts
+++ src/api/REST/GET/useGetRandomUser.ts
@@ -1,8 +1,8 @@
 // https://randomuser.me/
 
 import { useQuery, UseQueryOptions } from "@tanstack/react-query";
-import { z } from "zod";
 import { QueryKey, useRestFetcher } from "api/REST/useRestFetcher";
+import { z } from "zod";
 
 const requestShape = z.object({
   searchParams: z
@@ -15,10 +15,73 @@ const requestShape = z.object({
 type RequestShape = z.infer<typeof requestShape>;
 
 const responseShape = z.object({
-  results: z.array(z.any()),
-  info: z.record(z.any()),
+  results: z.array(
+    z.object({
+      gender: z.string(),
+      name: z.object({
+        title: z.string(),
+        first: z.string(),
+        last: z.string(),
+      }),
+      location: z.object({
+        street: z.object({
+          number: z.number(),
+          name: z.string(),
+        }),
+        city: z.string(),
+        state: z.string(),
+        country: z.string(),
+        postcode: z.union([z.string(), z.number()]).nullable(),
+        coordinates: z.object({
+          latitude: z.string(),
+          longitude: z.string(),
+        }),
+        timezone: z.object({
+          offset: z.string(),
+          description: z.string(),
+        }),
+      }),
+      email: z.string(),
+      login: z.object({
+        uuid: z.string(),
+        username: z.string(),
+        password: z.string(),
+        salt: z.string(),
+        md5: z.string(),
+        sha1: z.string(),
+        sha256: z.string(),
+      }),
+      dob: z.object({
+        date: z.string(),
+        age: z.number(),
+      }),
+      registered: z.object({
+        date: z.string(),
+        age: z.number(),
+      }),
+      phone: z.string(),
+      cell: z.string(),
+      id: z.object({
+        name: z.string(),
+        value: z.union([z.string(), z.number()]).nullable(),
+      }),
+      picture: z.object({
+        large: z.string(),
+        medium: z.string(),
+        thumbnail: z.string(),
+      }),
+      nat: z.string(),
+    })
+  ),
+  info: z.object({
+    seed: z.string(),
+    results: z.number(),
+    page: z.number(),
+    version: z.string(),
+  }),
 });
-type ResponseShape = z.infer<typeof responseShape>;
+
+export type ResponseShape = z.infer<typeof responseShape>;
 
 interface UseGetRandomUserProps<TResponseShape> {
   searchParams?: RequestShape["searchParams"];
